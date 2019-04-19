0a8a49959af63382b6132baaa151cedf62e8b6a2
Venkatagithub2019 <45400964+Pedereddla@users.noreply.github.com>
2019-04-19 02:13:57 +0000

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "Welcome to DevOps - 20th Apr 2019 - updated by Venkat" );
     }
 }
